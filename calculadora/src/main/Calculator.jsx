df8ff51d75fd56d28ef63d19e332f027e7b94e70
KailanySousa <kailany-sousa2010@hotmail.com>
2020-12-22 22:02:07 -0300

    componente calculator

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator">
+            </div>
+        )
+    }
+}
+
+export default Calculator;
