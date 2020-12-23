ac7682befc8b5feb2457f044c15716a6bd7465d5
KailanySousa <kailany-sousa2010@hotmail.com>
2020-12-22 22:51:53 -0300

    finalizando layout componente button

--- calculadora/src/component/Button/index.jsx
+++ calculadora/src/component/Button/index.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import './index.css';
 
 class Button extends Component {
+
     render() {
         return (
-            <button className="button">{this.props.label}</button>
+            <button className={`
+                button
+                ${this.props.operation ? 'operation' : ''}
+                ${this.props.double ? 'double' : ''}
+                ${this.props.triple ? 'triple' : ''}
+            `} onClick={e => this.props.click && this.props.click(this.props.label)}>
+                {this.props.label}
+            </button>
         )
     }
 }
--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -5,25 +5,40 @@ import Button from '../component/Button/index';
 import Display from '../component/Display/index';
 
 class Calculator extends Component {
+
+
+    clearMemory = () => {
+        console.log("limpar");
+    }
+
+    setOperation = (operation) => {
+        console.log(operation);
+    }
+
+    addDigit = (n) => {
+        console.log(n);
+    }
+
     render() {
+
         return (
             <div className="calculator">
-                <Display value={100}/>
-                <Button label="AC" />
-                <Button label="/" />
-                <Button label="7" />
-                <Button label="8" />
-                <Button label="9" />
-                <Button label="*" />
-                <Button label="4" />
-                <Button label="5" />
-                <Button label="6" />
-                <Button label="-" />
-                <Button label="1" />
-                <Button label="2" />
-                <Button label="3" />
-                <Button label="+" />
-                <Button label="0" />
+                <Display value={100} />
+                <Button label="AC" triple click={this.clearMemory} />
+                < Button label="/" operation click={this.setOperation} />
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" operation click={this.setOperation} />
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" operation click={this.setOperation} />
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" operation click={this.setOperation} />
+                <Button label="0" double click={this.addDigit} />
                 <Button label="." />
                 <Button label="=" />
             </div>
